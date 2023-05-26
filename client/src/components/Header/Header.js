599acad2df566c80e957910165d61a67d450e109
martincosimano <martincosimano@hotmail.com>
2023-05-26 15:29:19 -0300

    working on the hamburger navbar

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from './Navbar'
 // import { Link } from "react-router-dom";
 
 export default function Header() {
@@ -7,28 +8,7 @@ export default function Header() {
         <div className="logo-container">
             <a href=""><img src={require("../../assets/images/logo.png")} alt=""/></a>
         </div>
-
-        <button className="mobile-nav-toggle">
-            <span>Menu</span>
-        </button>
-
-        <nav>
-            <div className="navlist-flex">
-                <ul>
-                    <li><a href=""><span className="logo-text">Breaking Waves</span></a></li>
-                    <li><a href="" className="nav-anchor"><span>About</span></a></li>
-                    <li><a href="" className="nav-anchor"><span>Shop</span></a></li>
-                </ul>
-            </div>
-
-            <div className="navlist-flex">
-                <ul>
-                    <li><a href="" className="nav-anchor"><span><i className="fa-solid fa-cart-shopping nav-icon"></i></span></a></li>
-                    <li><a href="" className="nav-anchor"><span>Log In/Register</span></a></li>
-                </ul>
-            </div>
-
-        </nav>
+        <Navbar />
     </header>
     )
 }
--- client/src/components/Header/Navbar.js
+++ client/src/components/Header/Navbar.js
@@ -1,7 +1,46 @@
 import React, {useState} from 'react';
 
 export default function Navbar() {
+
+    const [burgerClass, setBurgerClass] = useState("burger-bar unclicked")
+    const [menuClass, setMenuClass] = useState("menu hidden")
+    const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+    function updateMenu() {
+        if(!isMenuClicked) {
+            setBurgerClass("burger-bar clicked")
+            setMenuClass("menu visible")
+        }
+        else {
+            setBurgerClass("burger-bar unclicked")
+            setMenuClass("menu hidden")
+        }
+    }
+
     return (
-        <h1></h1>
+        <div className="nav-container">
+            <nav>
+                <div className="burger-menu">
+                    <div className={burgerClass} onClick={updateMenu}></div>
+                    <div className={burgerClass} onClick={updateMenu}></div>
+                    <div className={burgerClass} onClick={updateMenu}></div>
+                </div>
+                <div className="navlist-flex">
+                    <ul>
+                        <li><a href=""><span className="logo-text">Breaking Waves</span></a></li>
+                        <li><a href="" className="nav-anchor"><span>About</span></a></li>
+                        <li><a href="" className="nav-anchor"><span>Shop</span></a></li>
+                    </ul>
+                </div>
+                <div className="navlist-flex">
+                    <ul>
+                        <li><a href="" className="nav-anchor"><span><i className="fa-solid fa-cart-shopping nav-icon"></i></span></a></li>
+                        <li><a href="" className="nav-anchor"><span>Log In/Register</span></a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div className={menuClass}></div>
+        </div>
     )
 }
