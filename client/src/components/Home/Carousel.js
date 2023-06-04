96ca9b4cb89c6d1222a39d807f54d855dcbc94c2
martincosimano <martincosimano@hotmail.com>
2023-06-04 14:49:51 -0300

    changes: -modified card size for smaller devices. -added 'useTransform' false to the carousel slider to improve slide animation on iOS chrome.

--- client/src/components/Home/Carousel.js
+++ client/src/components/Home/Carousel.js
@@ -21,30 +21,24 @@ export default function Carousel() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
         },
       },
       {
         breakpoint: 550,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        useTransform: false,
         },
       },
       {
-        breakpoint: 450,
+        breakpoint: 374,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 330,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         },
       },
     ],
