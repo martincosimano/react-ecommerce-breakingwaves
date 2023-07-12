4a42fc74636d10a9125b3e7025c66ac7ac22cc4a
Martin Cosimano <martincosimanodev@gmail.com>
2023-07-12 18:59:10 -0300

    changes: -separated login & registration form into 2 different components. -login & registration forms now working. *to do: *display errors on login & registration forms(validation).

--- client/src/components/Account/Login.js
+++ client/src/components/Account/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 
 export default function Login() {
     const [formData, setFormData] = React.useState({
@@ -14,6 +15,24 @@ export default function Login() {
             [e.target.name]: e.target.value,
         }));
     }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const userData = {
+            email,
+            password
+        }
+
+        try {
+            const response = await axios.post('/api/users/login', userData);
+            console.log(response.data);
+          } catch (error) {
+            console.error(error);
+          }
+
+    }
+
     return (
     <div className="login-container">
         <div>
@@ -21,27 +40,27 @@ export default function Login() {
         </div>
         <div className="login-form">
             <form>
-            <label className="account-label">Email Address *</label>                    
-                <input
-                className="account-input" 
-                type="email"
-                id="email" 
-                name="email" 
-                value={email}
-                placeholder="Email Address"
-                onChange={onChange}
-            />
-            <label className="account-label">Password *</label>                    
-                <input
-                className="account-input" 
-                type="text"
-                id="password" 
-                name="password" 
-                value={password}
-                placeholder="Password"
-                onChange={onChange}
-            />
-            <button className="btn-black account-btn"><span>Log In</span></button>                        
+                <label className="account-label">Email Address *</label>                    
+                    <input
+                    className="account-input" 
+                    type="email"
+                    id="email" 
+                    name="email" 
+                    value={email}
+                    placeholder="Email Address"
+                    onChange={onChange}
+                />
+                <label className="account-label">Password *</label>                    
+                    <input
+                    className="account-input" 
+                    type="text"
+                    id="password" 
+                    name="password" 
+                    value={password}
+                    placeholder="Password"
+                    onChange={onChange}
+                />
+                <button className="btn-black account-btn" onClick={onSubmit}><span>Log In</span></button>                        
             </form>
         </div>
     </div>        
--- client/src/components/Account/Register.js
+++ client/src/components/Account/Register.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 
 export default function Register() {
     const [formData, setFormData] = React.useState({
@@ -18,6 +19,30 @@ export default function Register() {
             [e.target.name]: e.target.value,
         }));
     }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        if(!firstName || !lastName || !email || !email2 || !password || !password2) {
+            console.log('Empty field')
+        }else if(password !== password2) {
+            console.log('Passwords do not match');
+        }else if(email !== email2) {
+            console.log('Email Address do not match')
+        }else {
+            const userData = {
+                name: `${firstName} ${lastName}`,
+                email,
+                password
+            }
+            try {
+                const response = await axios.post('/api/users', userData);
+                console.log(response.data);
+              } catch (error) {
+                console.error(error);
+              }
+        }
+    }
     
     return (
     <div className="registration-container">
@@ -26,67 +51,67 @@ export default function Register() {
         </div>
         <div className="register-form">
             <form>
-            <label className="account-label">First Name *</label>                    
-                <input
-                className="account-input" 
-                type="text"
-                id="firstName" 
-                name="firstName" 
-                value={firstName}
-                placeholder="First Name"
-                onChange={onChange}
-            />
-            <label className="account-label">Last Name *</label>                    
-                <input
-                className="account-input" 
-                type="text"
-                id="lastName" 
-                name="lastName" 
-                value={lastName}
-                placeholder="Last Name"
-                onChange={onChange}
-            />                        
-            <label className="account-label">Email Address *</label>                    
-                <input
-                className="account-input" 
-                type="email"
-                id="email" 
-                name="email" 
-                value={email}
-                placeholder="Email Address"
-                onChange={onChange}
-            />
-            <label className="account-label">Confirm Email Address *</label>                    
-                <input
-                className="account-input" 
-                type="email"
-                id="email2" 
-                name="email2" 
-                value={email2}
-                placeholder="Confirm Email Address"
-                onChange={onChange}
-            />
-            <label className="account-label">Password *</label>                    
-                <input
-                className="account-input" 
-                type="text"
-                id="password" 
-                name="password" 
-                value={password}
-                placeholder="Password"
-                onChange={onChange}
-            />
-            <label className="account-label">Confirm Password *</label>                    
-                <input
-                className="account-input" 
-                type="text"
-                id="password2" 
-                name="password2" 
-                value={password2}
-                placeholder="Confirm Password"
-                onChange={onChange}
-            />
-            <button className="btn-black account-btn"><span>Register</span></button>                        
+                <label className="account-label">First Name *</label>                    
+                    <input
+                    className="account-input" 
+                    type="text"
+                    id="firstName" 
+                    name="firstName" 
+                    value={firstName}
+                    placeholder="First Name"
+                    onChange={onChange}
+                />
+                <label className="account-label">Last Name *</label>                    
+                    <input
+                    className="account-input" 
+                    type="text"
+                    id="lastName" 
+                    name="lastName" 
+                    value={lastName}
+                    placeholder="Last Name"
+                    onChange={onChange}
+                />                        
+                <label className="account-label">Email Address *</label>                    
+                    <input
+                    className="account-input" 
+                    type="email"
+                    id="email" 
+                    name="email" 
+                    value={email}
+                    placeholder="Email Address"
+                    onChange={onChange}
+                />
+                <label className="account-label">Confirm Email Address *</label>                    
+                    <input
+                    className="account-input" 
+                    type="email"
+                    id="email2" 
+                    name="email2" 
+                    value={email2}
+                    placeholder="Confirm Email Address"
+                    onChange={onChange}
+                />
+                <label className="account-label">Password *</label>                    
+                    <input
+                    className="account-input" 
+                    type="text"
+                    id="password" 
+                    name="password" 
+                    value={password}
+                    placeholder="Password"
+                    onChange={onChange}
+                />
+                <label className="account-label">Confirm Password *</label>                    
+                    <input
+                    className="account-input" 
+                    type="text"
+                    id="password2" 
+                    name="password2" 
+                    value={password2}
+                    placeholder="Confirm Password"
+                    onChange={onChange}
+                />
+                <button className="btn-black account-btn" onClick={onSubmit}><span>Register</span></button>                        
             </form>
         </div>                    
     </div>
